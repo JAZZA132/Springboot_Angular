cfe971a65077efa0a7b23c27b99be21069363242
Wang_Aaron <j5224821120@i-mps.com>
2023-05-30 14:50:09 +0800

    Revert "aaaa"
    This reverts commit 8f20b6c8cdee31f1a8b3fe97f43a758fafdcfea8.

--- backend/src/main/java/backend/bean/Hello.java
+++ backend/src/main/java/backend/bean/Hello.java
@@ -12,7 +12,7 @@ public class Hello {
     public void setGreeting(String greeting) {
         this.greeting = greeting;
     }
-///aaaaaaa
+
     public String getGreeting() {
         return this.greeting;
     }
