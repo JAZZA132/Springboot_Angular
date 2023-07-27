c8fd046a944f41a5fd7f858f02f4a8e30369d037
Wang_Aaron <j5224821120@i-mps.com>
2023-07-27 09:55:07 +0800

    aaaaa

--- backend/src/main/java/backend/controller/WebController.java
+++ backend/src/main/java/backend/controller/WebController.java
@@ -17,4 +17,19 @@ public class WebController {
     public Hello greeting(User user) throws Exception {
         return new Hello("Hi, " + user.getName() + "!");
     }
+
+    @ApiOperation(value = "文件代號與版本")
+    @PostMapping("/doQueryDocNoVersion")
+    public ApiReturn<JSONObject> doQueryDocNoVersion() {
+        ApiReturn<JSONObject> result = new ApiReturn<>();
+
+        JSONObject data = new JSONObject();
+        data.put("docNoVersion", generalDaoService.selectList("sinopac_aip.dbo.cust.CUST_MF_DOC_INF.SQL5"));
+
+        result.setData(data);
+        EventLogUtil.recordEvent(EventTypeEnum.B_MF_DOC, EventActionEnum.QD, "文件代號與版本");
+
+        return result;
+    }
+    //test
 }
