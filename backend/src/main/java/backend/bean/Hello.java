661032b9e60ed250a53eb0b8fb2ba8965b480d40
Wang_Aaron <j5224821120@i-mps.com>
2023-07-27 09:58:18 +0800

    Revert "vvv"
    This reverts commit 763bae507b43636f085a2531583188934cfa7781.

--- backend/src/main/java/backend/bean/Hello.java
+++ backend/src/main/java/backend/bean/Hello.java
@@ -12,7 +12,7 @@ public class Hello {
     public void setGreeting(String greeting) {
         this.greeting = greeting;
     }
-
+///dddd
     public String getGreeting() {
         return this.greeting;
     }//111
